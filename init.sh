27887e76891067b17dcb0ea74f5adeeba9ba21f6
Matthew DeAbreu <Matthew.DeAbreu@gmail.com>
2025-02-12 19:22:41 -0800

    update node

--- init.sh
+++ init.sh
@@ -17,15 +17,15 @@ sudo apt install -y sqlite3
 sudo apt install -y prusa-slicer
 
 # Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install node
-nvm install v20.15.0
-nvm alias default v20.15.0
+nvm install v22.14.0
+nvm alias default v22.14.0
 
 # Install pnpm
 npm install -g pnpm
